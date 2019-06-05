a66998da5a5ccdd832041856c08e1a0284234ad3
chris <jinyoungcho93@gmail.com>
2019-06-05 13:25:44 +0900

    Mininum Spanning Tree

---
+++ PYTHON_CODE2/9372.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/9372
+
+# 상근이의 여행
+
+T = int(input())
+
+for t in range(T):
+
+    N, M = list(map(int, input().split()))
+
+    for _ in range(M):
+        input()
+
+    print(N-1)
